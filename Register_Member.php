5483e8050b12d6168461a3269f0371d29a8e706b
Daniel Jackson <danj210@hotmail.com>
2016-12-28 10:29:12 -0500

    Creating my first SQLite database to use with this web app.

--- Register_Member.php
+++ Register_Member.php
@@ -1,6 +1,25 @@
 <!DOCTYPE html>
 <html>
-<?php 
+    <?php 
+    /*
+    $firstName = $_POST["firstName"];
+    echo $firstName;
+    echo $_POST['lastName'];
+*/
+    $dsn = "sqlite:/Users/DanJ2/Documents/focarino.sqlite3";
+    try {
+    $conn = new PDO($dsn);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    } catch (PDOException $e) {
+        echo "Connection failes:".$e->getMessage();
+    }
+    $sql = "DROP TABLE IF EXISTS foc_member";
+    $conn->exec($sql);
+    $sql = "CREATE TABLE members (id INTEGER PRIMARY KEY, firstName, lastName)";
+    $conn->exec($sql);
 
-?>
+    // Adding a member
+    $sqladd = "INSERT INTO members"
+    ?>
 </html>
