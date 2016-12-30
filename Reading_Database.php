694c24d94cb834473c8579cd707a7091f998a90c
Daniel Jackson <danj210@hotmail.com>
2016-12-29 21:45:58 -0500

    Scrapping and starting over because SQLite isn't working like I wanted.

--- Reading_Database.php
+++ Reading_Database.php
@@ -7,16 +7,48 @@ require_once('C:/Wamp_64/www/Focarino_Database/config.php');
 $sql = "SELECT id from foc_member";
 $st = $conn->query($sql);
 $members = $st->fetch();
+/*
 echo "ID: ".$members["id"]."<br/>";
 echo "First Name:  ".$members["firstName"]."<br/>";
 echo "Last Name:  ".$members["lastName"]."<br/>";
 echo count($members);
+*/
+for ($x = 0; $x < 5; $x++) {
+    echo $x;
+    $sql = "SELECT * FROM foc_member WHERE id=$x";
+    echo $members["id"]."</br>";
+    echo $members["firstName"];
+}
+$count = 0;
+echo count($members);
+for ($index=0; $index<=count($members); $index++) {
+    $sql = "SELECT * FROM foc_member WHERE id=$index";
+    echo $members["id"]."</br>";
+    echo "This is the index: " . $index;
+    echo $count++;
+    //echo $members["firstName"]."</br>";
+    //echo $members["lastName"]."</br>";
+}
+//$sql = "SELECT * FROM foc_member WHERE id=10";
+//st = $conn->query($sql);
+//$testingID = $st->fetch();
+//echo $testingID["firstName"];
+//echo $testingID["lastName"];
+
+echo "</br>";
+
+for ($i=0; $i<12; $i++) {
+    $count++;
+    echo $members["id"];
+
+}
 foreach ($members as $member) {
+    /*
     echo $members["id"]."<br/>";
     echo $members["firstName"]."<br/>";
     echo $members["lastName"]."<br/>";
     //echo $members["lastName"];
-    
+    */
     //echo $conn->query("SELECT firstName from foc_member");
 }
 ?>
--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $dsn = "sqlite:/Users/DanJ2/Documents/focarino.sqlite3";
-    try {
-        $conn = new PDO($dsn);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Connection failes:".$e->getMessage();
-    }
-?>
