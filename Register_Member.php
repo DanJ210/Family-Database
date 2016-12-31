53e66d0793afe1f36cee9882a99d07c2ad1086df
Daniel Jackson <danj210@hotmail.com>
2016-12-31 07:46:21 -0500

    Commenting out almost all code to create a new SQLite3 class file and require it in register_member.

--- Register_Member.php
+++ Register_Member.php
@@ -5,6 +5,25 @@
     // Page holding database connection configurations 
     require_once('C:/Wamp_64/www/Focarino_Database/config.php');
     require_once('DataObject.class.php');
+
+    try {
+        $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+        $conn->setAttribute( PDO::ATTR_PERSISTENT, true );
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch ( PDOException $e ) {
+        die( "Connection Failed: ". $e->getMessage() );
+    }
+    // Getting data from register page
+    $fName = $_POST['firstName'];
+    $lName = $_POST['lastName'];
+    $bday = $_POST['birthDate'];
+
+    $sql = 'INSERT INTO (firstName, lastName, birthDate) VALUES (:fname, :lname, :bday)';
+    $st = $conn->prepare( $sql );
+    $st->bindValue( ":fname", $fName );
+    $st->bindValue( ":lname", $lName );
+    $st->bindValue( ":bday", $bday );
+    $st->execute();
     // Creating PDO connection
     /*
     $dsn = "sqlite:/Users/DanJ2/Documents/focarino.sqlite3";
@@ -25,7 +44,7 @@
     */
     
     // Adding a member
-    
+    /*
     $sqladd = "INSERT INTO foc_member (firstName, lastName) VALUES (:firstNameText,:lastNameText)";
     $st = $conn->prepare($sqladd);
     $st->bindValue(":firstNameText", $_POST['firstName']);
@@ -57,6 +76,7 @@
     else {
         echo "Please enter information again";
     }
+    */
     ?>
     <body>
         </br>
