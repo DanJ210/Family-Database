d747767478694ce006a05d1f7dda7856a4a6c9bb
Daniel Jackson <danj210@hotmail.com>
2017-01-01 23:57:24 -0500

    Changing how displaying works with a more complicated way but having an issue removing a while statement that has nothing to do with displaying.

--- Reading_Database.php
+++ Reading_Database.php
@@ -2,7 +2,7 @@
 <html>
     <style>
     dl {
-        display: inline;
+        display: block;
     }
     dt {
         background-color: cornflowerblue;
@@ -10,7 +10,7 @@
         text-align: center;
     }
     dd {
-        float: left;
+        /*float: left;*/
     }
     </style>
 <?php 
@@ -29,42 +29,56 @@ $sqlDisplay =<<<EOF
     SELECT * FROM family_members;
 EOF;
 ?>
-    <dl>
-        <dt> First Name</dt>
         
 <?php
 $results = $FcDB->query($sqlDisplay);
+$results -> fetchArray(SQLITE3_ASSOC);
+
+// Removing this while statement breaks display
+while ($row = $results->fetchArray(SQLITE3_ASSOC) ) {
+    echo "Testing";
+}
+/*
 while ($row = $results->fetchArray(SQLITE3_ASSOC) ) {
     ?>
         <dd><?php echo $row['firstname'] ?></dd>    
     
     <?php 
-}
+} */
 
 
 ?>
-    </dl>
-    </br>
-    </br>
-    </br>
-    <dt> Last Name</dt>
     <?php 
+    /*
     while ($row = $results->fetchArray(SQLITE3_ASSOC) ) {
     ?>
         <dd><?php echo $row['lastname'] ?></dd>    
     <?php 
-    }
+    } */
+    $count = 1;
     while ($row = $results->fetchArray(SQLITE3_ASSOC) ) {
-        echo "</br>";
-        echo "ID = " . $row['id'] . "</br>";
-        echo "First Name = " . $row['firstname'] . "</br>";
-        echo "Last Name = " . $row['lastname'] . "</br>";
-        echo "Bday = " . $row['birthdate'] . "</br>";
-        echo "City = " . $row['city'] . "</br>";
-        echo "State = " . $row['state'] . "</br>";
-        echo "Join Date = " . $row['joindate'] . "</br>";
-        echo "</br>";
-        echo "</br>";
+        ?>
+        <dl>
+            <dt>Record <?php echo $count;?></dt>
+                <dd>
+                <?php 
+                echo count($row);
+                echo "</br>";
+                echo "ID = " . $row['id'] . "</br>";
+                echo "First Name = " . $row['firstname'] . "</br>";
+                echo "Last Name = " . $row['lastname'] . "</br>";
+                echo "Bday = " . $row['birthdate'] . "</br>";
+                echo "City = " . $row['city'] . "</br>";
+                echo "State = " . $row['state'] . "</br>";
+                echo "Join Date = " . $row['joindate'] . "</br>";
+                echo "</br>";
+                echo "</br>";
+                ?>
+                </dd>
+        </dl>
+        <?php
+        // The count is for giving the record displayed a number
+        $count++;
     }
     $FcDB->close();
     ?>
